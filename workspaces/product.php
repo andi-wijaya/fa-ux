6f51dc43a7c59d83b71d84ba0d686733bdb0c38f
Andi Wijaya <it@floweradvisor.com>
2018-01-15 17:15:23 +0700

    Grid update

---
+++ workspaces/product.php
@@ -0,0 +1,58 @@
+<div class="content padding20">
+
+  <h1>Hello Product</h1>
+  <div></div>
+  <button id="loadme">Load Me</button>
+  <button id="getvalue">Get Value In Console</button>
+
+  <div class="height10"></div>
+
+  <span id="grid1head"></span>
+  <span id="grid1"></span>
+
+  <script>
+
+    var mod = {
+
+      columns:[
+        { name:"code", text:"Code", width:"100px" },
+        { name:"title", text:"Title", width:"200px" },
+        { name:"description", text:"Description", width:"300px" },
+        { name:"createdon", text:"Created ON", width:"100px" },
+      ],
+
+      init:function(){
+
+        $('#grid1head').gridhead({
+          class:"style1",
+          columns:mod.columns,
+          grid:"#grid1",
+        });
+
+        // Initialize grid1 with given attributes
+        $('#grid1').grid({
+          autoload:true,
+          class:"style1",
+          columns:mod.columns,
+          method:"get",
+          src:"../data/play1_datasource1.php",
+          row_per_page:20,
+          scroll_cont:'window',
+          moveable:true,
+          onselect:function(){
+            console.log(arguments);
+          },
+        });
+
+        $('#loadme').click(function(){ $('#grid1').grid_load(); }); // Load me button click handler
+        $('#getvalue').click(function(){ console.log($('#grid1').val()); }); // Get value button click handler
+
+      }
+
+    }
+
+    $(mod.init);
+
+  </script>
+
+</div>
